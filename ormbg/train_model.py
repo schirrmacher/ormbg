f1af846c4858204a04c0cb029182216238dadb9c
Marvin Schirrmacher <schirma@amazon.com>
2024-07-03 18:37:59 +0200

    cleanup model training code

--- ormbg/train_model.py
+++ ormbg/train_model.py
@@ -1,16 +1,16 @@
 import os
 import time
-import numpy as np
-from skimage import io
-import time
 
-import torch, gc
+import torch
 import torch.nn as nn
-from torch.autograd import Variable
 import torch.optim as optim
-import torch.nn.functional as F
+from torch.autograd import Variable
+
+import numpy as np
 
 from pathlib import Path
+
+from models.ormbg import ORMBG
 
 from data_loader_cache import (
     get_im_gt_name_dict,
@@ -18,8 +18,6 @@ from data_loader_cache import (
     GOSGridDropout,
     GOSRandomHFlip,
 )
-from basics import f1_mae_torch
-from models.ormbg import ORMBG
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -32,8 +30,6 @@ def train(
     valid_dataloaders,
     valid_datasets,
     hypar,
-    train_dataloaders_val,
-    train_datasets_val,
 ):
 
     model_path = hypar["model_path"]
@@ -59,7 +55,8 @@ def train(
     gos_dataloader = train_dataloaders[0]
     epoch_num = hypar["max_epoch_num"]
     notgood_cnt = 0
-    for epoch in range(epoch_num):  ## set the epoch num as 100000
+
+    for epoch in range(epoch_num):
 
         for i, data in enumerate(gos_dataloader):
 
@@ -188,172 +185,23 @@ def train(
     print("Training Reaches The Maximum Epoch Number")
 
 
-def valid(net, valid_dataloaders, valid_datasets, hypar, epoch=0):
-    net.eval()
-    print("Validating...")
-    epoch_num = hypar["max_epoch_num"]
+def main(train_datasets, valid_datasets, hypar):
 
-    val_loss = 0.0
-    tar_loss = 0.0
-    val_cnt = 0.0
+    print("--- create training dataloader ---")
 
-    tmp_f1 = []
-    tmp_mae = []
-    tmp_time = []
-
-    start_valid = time.time()
-
-    for k in range(len(valid_dataloaders)):
-
-        valid_dataloader = valid_dataloaders[k]
-        valid_dataset = valid_datasets[k]
-
-        val_num = valid_dataset.__len__()
-        mybins = np.arange(0, 256)
-        PRE = np.zeros((val_num, len(mybins) - 1))
-        REC = np.zeros((val_num, len(mybins) - 1))
-        F1 = np.zeros((val_num, len(mybins) - 1))
-        MAE = np.zeros((val_num))
-
-        for i_val, data_val in enumerate(valid_dataloader):
-            val_cnt = val_cnt + 1.0
-            imidx_val, inputs_val, labels_val, shapes_val = (
-                data_val["imidx"],
-                data_val["image"],
-                data_val["label"],
-                data_val["shape"],
-            )
-
-            if hypar["model_digit"] == "full":
-                inputs_val = inputs_val.type(torch.FloatTensor)
-                labels_val = labels_val.type(torch.FloatTensor)
-            else:
-                inputs_val = inputs_val.type(torch.HalfTensor)
-                labels_val = labels_val.type(torch.HalfTensor)
-
-            # wrap them in Variable
-            if torch.cuda.is_available():
-                inputs_val_v, labels_val_v = Variable(
-                    inputs_val.cuda(), requires_grad=False
-                ), Variable(labels_val.cuda(), requires_grad=False)
-            else:
-                inputs_val_v, labels_val_v = Variable(
-                    inputs_val, requires_grad=False
-                ), Variable(labels_val, requires_grad=False)
-
-            t_start = time.time()
-            ds_val = net(inputs_val_v)[0]
-            t_end = time.time() - t_start
-            tmp_time.append(t_end)
-
-            # loss2_val, loss_val = muti_loss_fusion(ds_val, labels_val_v)
-            loss2_val, loss_val = net.compute_loss(ds_val, labels_val_v)
-
-            # compute F measure
-            for t in range(hypar["batch_size_valid"]):
-                i_test = imidx_val[t].data.numpy()
-
-                pred_val = ds_val[0][t, :, :, :]  # B x 1 x H x W
-
-                ## recover the prediction spatial size to the orignal image size
-                pred_val = torch.squeeze(
-                    F.upsample(
-                        torch.unsqueeze(pred_val, 0),
-                        (shapes_val[t][0], shapes_val[t][1]),
-                        mode="bilinear",
-                    )
-                )
-
-                # pred_val = normPRED(pred_val)
-                ma = torch.max(pred_val)
-                mi = torch.min(pred_val)
-                pred_val = (pred_val - mi) / (ma - mi)  # max = 1
-
-                if len(valid_dataset.dataset["ori_gt_path"]) != 0:
-                    gt = np.squeeze(
-                        io.imread(valid_dataset.dataset["ori_gt_path"][i_test])
-                    )  # max = 255
-                    if gt.max() == 1:
-                        gt = gt * 255
-                else:
-                    gt = np.zeros((shapes_val[t][0], shapes_val[t][1]))
-                with torch.no_grad():
-                    gt = torch.tensor(gt).to(device)
-
-                pre, rec, f1, mae = f1_mae_torch(
-                    pred_val * 255, gt, valid_dataset, i_test, mybins, hypar
-                )
-
-                PRE[i_test, :] = pre
-                REC[i_test, :] = rec
-                F1[i_test, :] = f1
-                MAE[i_test] = mae
-
-                del ds_val, gt
-                gc.collect()
-                torch.cuda.empty_cache()
-
-            # if(loss_val.data[0]>1):
-            val_loss += loss_val.item()  # data[0]
-            tar_loss += loss2_val.item()  # data[0]
-
-            print(
-                "[validating: %5d/%5d] val_ls:%f, tar_ls: %f, f1: %f, mae: %f, time: %f"
-                % (
-                    i_val,
-                    val_num,
-                    val_loss / (i_val + 1),
-                    tar_loss / (i_val + 1),
-                    np.amax(F1[i_test, :]),
-                    MAE[i_test],
-                    t_end,
-                )
-            )
-
-            del loss2_val, loss_val
-
-        print("============================")
-        PRE_m = np.mean(PRE, 0)
-        REC_m = np.mean(REC, 0)
-        f1_m = (1 + 0.3) * PRE_m * REC_m / (0.3 * PRE_m + REC_m + 1e-8)
-
-        tmp_f1.append(np.amax(f1_m))
-        tmp_mae.append(np.mean(MAE))
-
-    return tmp_f1, tmp_mae, val_loss, tar_loss, i_val, tmp_time
-
-
-def main(train_datasets, valid_datasets, hypar):  # model: "train", "test"
-
-    ### --- Step 1: Build datasets and dataloaders ---
-
-    if hypar["mode"] == "train":
-        print("--- create training dataloader ---")
-        ## collect training dataset
-        train_nm_im_gt_list = get_im_gt_name_dict(train_datasets, flag="train")
-        ## build dataloader for training datasets
-        train_dataloaders, train_datasets = create_dataloaders(
-            train_nm_im_gt_list,
-            cache_size=hypar["cache_size"],
-            cache_boost=hypar["cache_boost_train"],
-            my_transforms=[GOSGridDropout(), GOSRandomHFlip()],
-            batch_size=hypar["batch_size_train"],
-            shuffle=True,
-        )
-        train_dataloaders_val, train_datasets_val = create_dataloaders(
-            train_nm_im_gt_list,
-            cache_size=hypar["cache_size"],
-            cache_boost=hypar["cache_boost_train"],
-            my_transforms=[],
-            batch_size=hypar["batch_size_valid"],
-            shuffle=False,
-        )
-        print(len(train_dataloaders), " train dataloaders created")
-
-    print("--- create valid dataloader ---")
-    ## build dataloader for validation or testing
-    valid_nm_im_gt_list = get_im_gt_name_dict(valid_datasets, flag="valid")
+    train_nm_im_gt_list = get_im_gt_name_dict(train_datasets, flag="train")
     ## build dataloader for training datasets
+    train_dataloaders, train_datasets = create_dataloaders(
+        train_nm_im_gt_list,
+        cache_size=hypar["cache_size"],
+        cache_boost=hypar["cache_boost_train"],
+        my_transforms=[GOSGridDropout(), GOSRandomHFlip()],
+        batch_size=hypar["batch_size_train"],
+        shuffle=True,
+    )
+
+    valid_nm_im_gt_list = get_im_gt_name_dict(valid_datasets, flag="valid")
+
     valid_dataloaders, valid_datasets = create_dataloaders(
         valid_nm_im_gt_list,
         cache_size=hypar["cache_size"],
@@ -362,14 +210,9 @@ def main(train_datasets, valid_datasets, hypar):  # model: "train", "test"
         batch_size=hypar["batch_size_valid"],
         shuffle=False,
     )
-    print(len(valid_dataloaders), " valid dataloaders created")
-    # print(valid_datasets[0]["data_name"])
 
-    ### --- Step 2: Build Model and Optimizer ---
-    print("--- build model ---")
-    net = hypar["model"]  # GOSNETINC(3,1)
+    net = hypar["model"]
 
-    # convert to half precision
     if hypar["model_digit"] == "half":
         net.half()
         for layer in net.modules():
@@ -394,31 +237,22 @@ def main(train_datasets, valid_datasets, hypar):  # model: "train", "test"
                 )
             )
 
-    print("--- define optimizer ---")
     optimizer = optim.Adam(
         net.parameters(), lr=1e-3, betas=(0.9, 0.999), eps=1e-08, weight_decay=0
     )
 
-    ### --- Step 3: Train or Valid Model ---
-    if hypar["mode"] == "train":
-        train(
-            net,
-            optimizer,
-            train_dataloaders,
-            train_datasets,
-            valid_dataloaders,
-            valid_datasets,
-            hypar,
-            train_dataloaders_val,
-            train_datasets_val,
-        )
-    else:
-        valid(net, valid_dataloaders, valid_datasets, hypar)
+    train(
+        net,
+        optimizer,
+        train_dataloaders,
+        train_datasets,
+        valid_dataloaders,
+        valid_datasets,
+        hypar,
+    )
 
 
 if __name__ == "__main__":
-
-    ### --------------- STEP 1: Configuring the Train, Valid and Test datasets ---------------
 
     output_model_folder = "saved_models"
     Path(output_model_folder).mkdir(parents=True, exist_ok=True)
@@ -450,93 +284,51 @@ if __name__ == "__main__":
     ### --------------- STEP 2: Configuring the hyperparamters for Training, validation and inferencing ---------------
     hypar = {}
 
-    ## -- 2.1. configure the model saving or restoring path --
-    hypar["mode"] = "train"
-    ## "train": for training,
-    ## "valid": for validation and inferening,
-    ## in "valid" mode, it will calculate the accuracy as well as save the prediciton results into the "hypar["valid_out_dir"]", which shouldn't be ""
-    ## otherwise only accuracy will be calculated and no predictions will be saved
-    hypar["interm_sup"] = (
-        False  ## in-dicate if activate intermediate feature supervision
-    )
-
-    if hypar["mode"] == "train":
-        hypar["valid_out_dir"] = (
-            ""  ## for "train" model leave it as "", for "valid"("inference") mode: set it according to your local directory
-        )
-        hypar["model_path"] = (
-            "saved_models"  ## model weights saving (or restoring) path
-        )
-        hypar["restore_model"] = (
-            ""  ## name of the segmentation model weights .pth for resume training process from last stop or for the inferencing
-        )
-        hypar["start_ite"] = (
-            0  ## start iteration for the training, can be changed to match the restored training process
-        )
-        hypar["gt_encoder_model"] = ""
-    else:  ## configure the segmentation output path and the to-be-used model weights path
-        hypar["valid_out_dir"] = (
-            "../your-results/"  ##"../DIS5K-Results-test" ## output inferenced segmentation maps into this fold
-        )
-        hypar["model_path"] = output_model_folder
-        hypar["restore_model"] = "isnet.pth"
-
-    # if hypar["restore_model"]!="":
-    #     hypar["start_ite"] = int(hypar["restore_model"].split("_")[2])
-
-    ## -- 2.2. choose floating point accuracy --
-    hypar["model_digit"] = (
-        "full"  ## indicates "half" or "full" accuracy of float number
-    )
+    hypar["model"] = ORMBG()
     hypar["seed"] = 0
 
-    ## -- 2.3. cache data spatial size --
+    ## model weights path
+    hypar["model_path"] = "saved_models"
+
+    ## name of the segmentation model weights .pth for resume training process from last stop or for the inferencing
+    hypar["restore_model"] = ""
+
+    ## start iteration for the training, can be changed to match the restored training process
+    hypar["start_ite"] = 0
+
+    ## indicates "half" or "full" accuracy of float number
+    hypar["model_digit"] = "full"
+
     ## To handle large size input images, which take a lot of time for loading in training,
     #  we introduce the cache mechanism for pre-convering and resizing the jpg and png images into .pt file
     hypar["cache_size"] = [
         1024,
         1024,
-    ]  ## cached input spatial resolution, can be configured into different size
-    hypar["cache_boost_train"] = (
-        False  ## "True" or "False", indicates wheather to load all the training datasets into RAM, True will greatly speed the training process while requires more RAM
-    )
-    hypar["cache_boost_valid"] = (
-        False  ## "True" or "False", indicates wheather to load all the validation datasets into RAM, True will greatly speed the training process while requires more RAM
-    )
+    ]
 
-    ## --- 2.4. data augmentation parameters ---
-    hypar["input_size"] = [
-        1024,
-        1024,
-    ]  ## mdoel input spatial size, usually use the same value hypar["cache_size"], which means we don't further resize the images
-    hypar["crop_size"] = [
-        1024,
-        1024,
-    ]  ## random crop size from the input, it is usually set as smaller than hypar["cache_size"], e.g., [920,920] for data augmentation
-    hypar["random_flip_h"] = (
-        1  ## horizontal flip, currently hard coded in the dataloader and it is not in use
-    )
-    hypar["random_flip_v"] = 0  ## vertical flip , currently not in use
+    ## cached input spatial resolution, can be configured into different size
+    ## "True" or "False", indicates wheather to load all the training datasets into RAM, True will greatly speed the training process while requires more RAM
+    hypar["cache_boost_train"] = False
 
-    ## --- 2.5. define model  ---
-    print("building model...")
-    hypar["model"] = ORMBG()
-    hypar["early_stop"] = (
-        20  ## stop the training when no improvement in the past 20 validation periods, smaller numbers can be used here e.g., 5 or 10.
-    )
-    hypar["model_save_fre"] = (
-        2000  ## valid and save model weights every 2000 iterations
-    )
+    ## "True" or "False", indicates wheather to load all the validation datasets into RAM, True will greatly speed the training process while requires more RAM
+    hypar["cache_boost_valid"] = False
 
-    hypar["batch_size_train"] = 8  ## batch size for training
-    hypar["batch_size_valid"] = 1  ## batch size for validation and inferencing
-    print("batch size: ", hypar["batch_size_train"])
+    ## stop the training when no improvement in the past 20 validation periods, smaller numbers can be used here e.g., 5 or 10.
+    hypar["early_stop"] = 20
 
-    hypar["max_ite"] = (
-        10000000  ## if early stop couldn't stop the training process, stop it by the max_ite_num
-    )
-    hypar["max_epoch_num"] = (
-        1000000  ## if early stop and max_ite couldn't stop the training process, stop it by the max_epoch_num
-    )
+    ## valid and save model weights every 2000 iterations
+    hypar["model_save_fre"] = 2000
+
+    ## batch size for training
+    hypar["batch_size_train"] = 8
+
+    ## batch size for validation and inferencing
+    hypar["batch_size_valid"] = 1
+
+    ## if early stop couldn't stop the training process, stop it by the max_ite_num
+    hypar["max_ite"] = 10000000
+
+    ## if early stop and max_ite couldn't stop the training process, stop it by the max_epoch_num
+    hypar["max_epoch_num"] = 1000000
 
     main(train_datasets, valid_datasets, hypar=hypar)
