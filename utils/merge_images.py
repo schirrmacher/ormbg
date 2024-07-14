52a878e26e8fa52542a2ee60c94cd4eddbdaa05a
Marvin Schirrmacher <schirma@amazon.com>
2024-07-14 07:37:49 +0200

    iterate merge script

--- utils/merge_images.py
+++ utils/merge_images.py
@@ -3,7 +3,6 @@ import cv2
 import argparse
 import random
 import string
-import albumentations as A
 
 
 def create_ground_truth_mask(image):
@@ -16,8 +15,16 @@ def create_random_filename_from_filepath(path):
     return random_string + "_" + os.path.basename(path)
 
 
-def merge_images(background, foreground, position=(0, 0)):
+def resize_background_if_needed(background, foreground):
+    bh, bw = background.shape[:2]
+    fh, fw = foreground.shape[:2]
 
+    if bh != fh or bw != fw:
+        background = cv2.resize(background, (fw, fh), interpolation=cv2.INTER_AREA)
+    return background
+
+
+def merge_images(background, foreground, position=(0, 0)):
     x, y = position
 
     fh, fw = foreground.shape[:2]
@@ -47,44 +54,72 @@ def merge_images(background, foreground, position=(0, 0)):
 
 
 def create_training_data(
-    background_path, segmentation_path, image_path, ground_truth_path
+    background_dir, segmentation_dir, image_path, ground_truth_path
 ):
-    background = cv2.imread(background_path, cv2.IMREAD_COLOR)
-    segmentation = cv2.imread(segmentation_path, cv2.IMREAD_UNCHANGED)
+    if not os.path.exists(image_path):
+        os.makedirs(image_path)
+    if not os.path.exists(ground_truth_path):
+        os.makedirs(ground_truth_path)
 
-    if segmentation.shape[2] < 4:
-        raise Exception(f"Image does not have an alpha channel: {segmentation_path}")
+    background_files = [
+        os.path.join(background_dir, f)
+        for f in os.listdir(background_dir)
+        if os.path.isfile(os.path.join(background_dir, f))
+    ]
+    segmentation_files = [
+        os.path.join(segmentation_dir, f)
+        for f in os.listdir(segmentation_dir)
+        if os.path.isfile(os.path.join(segmentation_dir, f))
+    ]
 
-    file_name = create_random_filename_from_filepath(segmentation_path)
-    image_path = os.path.join(image_path, file_name)
-    ground_truth_path = os.path.join(ground_truth_path, file_name)
+    for segmentation_path in segmentation_files:
+        segmentation = cv2.imread(segmentation_path, cv2.IMREAD_UNCHANGED)
+        if segmentation.shape[2] < 4:
+            raise Exception(
+                f"Image does not have an alpha channel: {segmentation_path}"
+            )
 
-    ground_truth = create_ground_truth_mask(segmentation)
-    result = merge_images(background, segmentation)
+        background_path = random.choice(background_files)
+        background = cv2.imread(background_path, cv2.IMREAD_COLOR)
 
-    assert ground_truth.shape[0] == result.shape[0]
-    assert ground_truth.shape[1] == result.shape[1]
+        background = resize_background_if_needed(background, segmentation)
 
-    cv2.imwrite(ground_truth_path, ground_truth)
-    cv2.imwrite(image_path, result)
+        file_name = create_random_filename_from_filepath(segmentation_path)
+        image_output_path = os.path.join(image_path, file_name)
+        ground_truth_output_path = os.path.join(ground_truth_path, file_name)
+
+        ground_truth = create_ground_truth_mask(segmentation)
+        result = merge_images(background, segmentation)
+
+        assert ground_truth.shape[0] == result.shape[0]
+        assert ground_truth.shape[1] == result.shape[1]
+
+        cv2.imwrite(ground_truth_output_path, ground_truth)
+        cv2.imwrite(image_output_path, result)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Merge two images with one image having transparency."
+        description="Merge images in folders with one image having transparency."
     )
     parser.add_argument(
-        "-b", "--background", required=True, help="Path to the background image"
+        "-bd",
+        "--background-dir",
+        required=True,
+        help="Path to the background images directory",
     )
     parser.add_argument(
-        "-s", "--segmentation", required=True, help="Path to the segmentation image"
+        "-sd",
+        "--segmentation-dir",
+        required=True,
+        help="Path to the segmentation images directory",
     )
     parser.add_argument(
         "-im",
         "--image-path",
         type=str,
         default="im",
-        help="Path where the merged image will be saved",
+        help="Path where the merged images will be saved",
     )
     parser.add_argument(
         "-gt",
@@ -95,14 +130,9 @@ def main():
     )
     args = parser.parse_args()
 
-    if not os.path.exists(args.image_path):
-        os.makedirs(args.image_path)
-    if not os.path.exists(args.groundtruth_path):
-        os.makedirs(args.groundtruth_path)
-
     create_training_data(
-        background_path=args.background,
-        segmentation_path=args.segmentation,
+        background_dir=args.background_dir,
+        segmentation_dir=args.segmentation_dir,
         image_path=args.image_path,
         ground_truth_path=args.groundtruth_path,
     )
