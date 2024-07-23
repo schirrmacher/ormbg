4173c5ac62b2ab0c4afb51180fc6bb566448046c
Marvin Schirrmacher <schirma@amazon.com>
2024-07-23 22:35:02 +0200

    iterate trimap script

--- utils/trimap.py
+++ utils/trimap.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import argparse
+import os
 
 
 def load_image(image_path):
@@ -28,7 +29,7 @@ def save_image(image, output_path):
     cv2.imwrite(output_path, image)
 
 
-def main(image_path, threshold1, threshold2, output_path, width):
+def process_image(image_path, threshold1, threshold2, output_dir, width):
     image = load_image(image_path)
     if image is None:
         print(f"Error: Unable to load image from path: {image_path}")
@@ -37,15 +38,32 @@ def main(image_path, threshold1, threshold2, output_path, width):
     edges = apply_canny_edge_detection(image, threshold1, threshold2)
     result_image = draw_wide_edges_on_image(image, edges, width)
 
+    filename = os.path.basename(image_path)
+    output_path = os.path.join(output_dir, filename)
     save_image(result_image, output_path)
     print(f"Result image saved to {output_path}")
 
 
+def main(image_path, threshold1, threshold2, output_dir, width):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if os.path.isdir(image_path):
+        for filename in os.listdir(image_path):
+            file_path = os.path.join(image_path, filename)
+            if os.path.isfile(file_path):
+                process_image(file_path, threshold1, threshold2, output_dir, width)
+    else:
+        process_image(image_path, threshold1, threshold2, output_dir, width)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Apply Canny edge detection to an image and save the result."
+        description="Apply Canny edge detection to an image or a folder of images and save the result."
     )
-    parser.add_argument("image_path", type=str, help="Path to the image file.")
+    parser.add_argument(
+        "image_path", type=str, help="Path to the image file or folder."
+    )
     parser.add_argument(
         "--threshold1",
         type=int,
@@ -59,14 +77,14 @@ if __name__ == "__main__":
         help="Second threshold for the hysteresis procedure.",
     )
     parser.add_argument(
-        "--output",
+        "--output_dir",
         type=str,
-        default="result_image.png",
-        help="Path to save the result image.",
+        default="output",
+        help="Directory to save the result images.",
     )
     parser.add_argument(
         "--width", type=int, default=20, help="Width of the path around the edges."
     )
     args = parser.parse_args()
 
-    main(args.image_path, args.threshold1, args.threshold2, args.output, args.width)
+    main(args.image_path, args.threshold1, args.threshold2, args.output_dir, args.width)
